4c9602f4a2a8c7f2cb218985aa3ff38918674ef4
srgmnagar <nagarsrgm@gmail.com>
2024-09-10 13:38:36 +0530

    used axios for fetching

--- task1/script.js
+++ task1/script.js
@@ -1,10 +1,9 @@
-const url = 'https://dummyjson.com/posts?limit=10';
+const url = 'https://dummyjson.com/posts';
 const postForm = document.querySelector("#postForm");
 const title = document.querySelector("#title");
 const body = document.querySelector("#body");
 const likes = document.querySelector("#likes");
 const dislikes = document.querySelector("#dislikes");
-const views = document.querySelector("#views");
 const userId = document.querySelector("#userId");
 let currentPostId = null;
 // Fetch posts
@@ -29,7 +28,6 @@ function displayEvents(data) {
           <h1 class="bodyC font-medium text-slate-600 ">${data.body} </h1>
           <p class="likesC font-medium text-blue-900">Likes: ${data.reactions.likes}</p>
           <p class="dislikesC font-medium text-blue-900">Dislikes: ${data.reactions.dislikes}</p>
-          <p class="viewsC font-medium  text-blue-900">Views: ${data.views}</p>
           <p class="userIdC font-medium  text-blue-900">User ID: ${data.userId}</p>
           <div class="py-2 font-medium text-slate-900 flex gap-2 md:gap-4">
               <button data-id="${data.id}" class="update btn-primary transition duration-300 ease-in-out bg-blue-500 hover:bg-blue-600 text-white font-medium py-1 px-2 md:px-2 rounded-lg">Update</button>
@@ -46,14 +44,16 @@ function displayEvents(data) {
             e.preventDefault();
             const postId = e.target.getAttribute('data-id');
             const postCard = e.target.closest('.card');
-            fetch(`https://dummyjson.com/posts/${postId}`, {
-                method: 'DELETE',
-            })
-                .then(res => res.json())
-                .then(data => {
+
+            axios.delete(`https://dummyjson.com/posts/${postId}`)
+                .then(({ data }) => {
                     console.log(data);
                     postCard.remove();
+                })
+                .catch(error => {
+                    console.error(error);
                 });
+
         });
     });
 
@@ -67,7 +67,6 @@ function displayEvents(data) {
             const bodyC = postCard.querySelector(".bodyC").textContent;
             const likesC = parseInt(postCard.querySelector(".likesC").textContent.split(": ")[1]);
             const dislikesC = parseInt(postCard.querySelector(".dislikesC").textContent.split(": ")[1]);
-            const viewsC = parseInt(postCard.querySelector(".viewsC").textContent.split(": ")[1]);
             const userIdC = postCard.querySelector(".userIdC").textContent.split(": ")[1];
 
             console.log(titleC);
@@ -75,7 +74,6 @@ function displayEvents(data) {
             body.value = bodyC;
             likes.value = likesC;
             dislikes.value = dislikesC;
-            views.value = viewsC;
             userId.value = userIdC;
         });
     });
@@ -92,47 +90,43 @@ postForm.addEventListener("submit", (e) => {
             likes: likes.value,
             dislikes: dislikes.value,
         },
-        views: views.value,
         userId: userId.value,
     };
 
     if (currentPostId) {
-        fetch(`https://dummyjson.com/posts/${currentPostId}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(postData),
-        })
-            .then(res => res.json())
-            .then(data => {
-                alert("Post Updated");
-                const updatedCard = document.querySelector(`div[data-id="${currentPostId}"]`);
-                updatedCard.querySelector(".titleC").textContent = data.title;
-                updatedCard.querySelector(".bodyC").textContent = data.body;
-                updatedCard.querySelector(".likesC").textContent = `Likes: ${data.reactions.likes}`;
-                updatedCard.querySelector(".dislikesC").textContent = `Dislikes: ${data.reactions.dislikes}`;
-                updatedCard.querySelector(".viewsC").textContent = `Views: ${data.views}`;
-                updatedCard.querySelector(".userIdC").textContent = `User ID: ${data.userId}`;
-                currentPostId = null;
-            });
+        axios.put(`https://dummyjson.com/posts/${currentPostId}`, postData)
+    .then(({ data }) => {
+        alert("Post Updated");
+        const updatedCard = document.querySelector(`div[data-id="${currentPostId}"]`);
+        updatedCard.querySelector(".titleC").textContent = data.title;
+        updatedCard.querySelector(".bodyC").textContent = data.body;
+        updatedCard.querySelector(".likesC").textContent = `Likes: ${data.reactions.likes}`;
+        updatedCard.querySelector(".dislikesC").textContent = `Dislikes: ${data.reactions.dislikes}`;
+        updatedCard.querySelector(".userIdC").textContent = `User ID: ${data.userId}`;
+        currentPostId = null;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
     } else {
-        fetch('https://dummyjson.com/posts/add', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(postData),
-        })
-            .then(res => res.json())
-            .then(data => {
+
+        axios.post('https://dummyjson.com/posts/add', postData)
+            .then(({ data }) => {
                 console.log(data);
                 displayEvents([data]);
                 alert("Post Added");
+            })
+            .catch(error => {
+                console.error(error);
             });
+
     }
 
     title.value = '';
     body.value = '';
     likes.value = '';
     dislikes.value = '';
-    views.value = '';
     userId.value = '';
 });
 
